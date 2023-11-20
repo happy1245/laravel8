3774e33caad582aeac23469805e5fa2481faa796
happy1245 <jirapat.kerd@vru.ac.th>
2023-11-20 12:01:11 +0700

    Initial commit

---
+++ resources/views/test/cat.blade.php
@@ -0,0 +1,8 @@
+<html>
+
+<body>
+   <h1>gallery of jirapat 60xxxxxxxx</h1>
+   <h1><img src={{$cat}}></h1>
+</body>
+
+</html>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+    <p1>ant</p1><a href="gallery/ant"><img src={{$ant}} width="300"></a>
+    <p1>bird</p1><a href="gallery/bird"><img src={{$bird}} width="300"></a>
+    <p1>cat</p1><a href="gallery/cat"><img src={{$cat}} width="300"></a>
+    <p1>god</p1></h1><img src={{$god}} width="300">
+    <p1>spider</p1></h1><img src={{$spider}} width="300">
+</body>
+
+</html>
