9edae5c53299b68e0c5c965a857316c52da0f56d
happy1245 <jirapat.kerd@vru.ac.th>
2024-01-15 11:47:24 +0700

    week 7

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,6 +13,10 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index(Request $request)
     {
         // GET SEARCH KEYWORD
--- routes/web copy.php
+++ routes/web copy.php
@@ -67,16 +67,13 @@ Route::get("/gallery/cat", function () {
     $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
     return view("test/cat", compact("cat"));
 });
-
-Route::middleware(['auth', 'role:admin,teacher,student'])->group(function () {
 Route::get("/teacher", function () {
     return view("teacher");
-});
 });
 
 Route::get("/student", function () {
     return view("student");
-})->middleware('auth');
+});
 
 Route::get("/theme", function () {
     return view("theme");
@@ -151,9 +148,3 @@ Route::get('/covid19', [Covid19Controller::class, "index"]);
 Route::resource('/product', ProductController::class);
 
 Route::resource('/staff', StaffController::class);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__ . '/auth.php';
